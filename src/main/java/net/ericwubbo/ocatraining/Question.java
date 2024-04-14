b586977e2ca11532ee999d9db1777c88760e8b6f
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-04-14 13:37:55 +0200

    Start deel 5

---
+++ src/main/java/net/ericwubbo/ocatraining/Question.java
@@ -0,0 +1,13 @@
+package net.ericwubbo.ocatraining;
+
+public class Question {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Question(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/net/ericwubbo/ocatraining/QuestionController.java
+++ src/main/java/net/ericwubbo/ocatraining/QuestionController.java
@@ -1,13 +1,21 @@
 package net.ericwubbo.ocatraining;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("api/v1/questions")
 public class QuestionController {
+
     @GetMapping
-    public String test() {
-        return "Dit is een test van de QuestionController";
+    public List<Question> getAll() {
+        return List.of(
+                new Question("Which access modifier confers the smallest visibility?"),
+                new Question("What is the difference between overriding and overloading?"));
     }
 
     @GetMapping("second")
